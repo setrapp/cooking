296e8df12252404ddae7c8d8482d9a4f261eb0f0
Robert  Lumsden <rml8300@rit.edu>
2013-11-16 18:38:18 -0500

    New scene, 1st person, onmouseover script

---
+++ Assets/Scripts/Flowerscript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flowerscript : MonoBehaviour {
+	
+	public bool touched;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if(touched == true)
+		{
+			renderer.material.color = Color.blue;
+		}
+		else if (touched == false)
+		{
+			renderer.material.color = Color.green;
+		}
+	
+	}
+	
+	void OnMouseOver ()
+	{
+		touched = true;
+	}
+	
+	void OnMouseExit ()
+	{
+		touched = false;
+	}
+	
+}
