db9fa2e0c68cbe1aba375a0037615cee0ca306c2
unknown <set9069@SP2530-4.main.ad.rit.edu>
2013-12-17 08:52:50 -0500

    Fixess?

---
+++ Assets/Scripts/HideInRelativistic.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HideInRelativistic : MonoBehaviour {
+	public bool gotHidden = false;
+	public GameObject toHide = null;
+	private MovementScripts movement = null;
+	
+	// Use this for initialization
+	void Start () {
+		GameObject player = GameObject.FindGameObjectWithTag("Player");
+		if (player != null) {
+			movement = player.GetComponent<MovementScripts>();	
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (movement.IsRelativistic) {
+			toHide.SetActive(false);
+		} else {
+			toHide.SetActive(true);
+		}
+	}
+}
--- Assets/Scripts/RelativityLimiter.cs
+++ Assets/Scripts/RelativityLimiter.cs
@@ -7,17 +7,19 @@ public class RelativityLimiter : MonoBehaviour
  	public bool disableRelativity = true;
 	
 	void OnTriggerEnter(Collider collider) {
-		if (mover == null) {
-			GameObject player = GameObject.FindGameObjectWithTag("Player");
-			if (player != null) {
-				mover = player.GetComponent<MovementScripts>();
-			}
+		if (collider.gameObject.tag.Equals("PlayerTrigger")) {
 			if (mover == null) {
-				return;	
+				GameObject player = GameObject.FindGameObjectWithTag("Player");
+				if (player != null) {
+					mover = player.GetComponent<MovementScripts>();
+				}
+				if (mover == null) {
+					return;	
+				}
 			}
+			mover.relativityAvailable = !disableRelativity;
+			mover.ToggleSpecialRelativity(true, false);
 		}
-		mover.relativityAvailable = !disableRelativity;
-		mover.ToggleSpecialRelativity(true, false);
 	}
 }
 
