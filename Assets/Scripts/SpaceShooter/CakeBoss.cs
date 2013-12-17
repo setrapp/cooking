7d5e1de83ce17a4d733faf5feea63f7bdfa28ef2
darkfall <darkfall@gamemastercn.com>
2013-12-17 05:51:26 -0500

    updating spaceshooter minigame

--- Assets/Scripts/SpaceShooter/CakeBoss.cs
+++ Assets/Scripts/SpaceShooter/CakeBoss.cs
@@ -11,6 +11,8 @@ class CakeBoss: Enemy {
 	public override void OnEnabled() {
 		base.OnEnabled();
 		Scroller.Instance.OtherScrollObjects.Add (this.gameObject);
+
+		HUDLayer.Instance.ShowScreenText("Eat Eat Eat!!!", 0.5f, 2.0f, 0.5f, TextAnchor.UpperLeft);
 	}
 	
 	public override void OnDisabled() {
@@ -21,6 +23,8 @@ class CakeBoss: Enemy {
 	public override void OnDied() {
 		base.OnDied();
 		Scroller.Instance.OtherScrollObjects.Remove (this.gameObject);
+
+		HUDLayer.Instance.ShowScreenText("Thank you!!!!Press X to exit", 0.5f, 99999f, 0.5f, TextAnchor.UpperCenter);
 	}
 
 };
--- Assets/Scripts/SpaceShooter/SatelliteShooter.cs
+++ Assets/Scripts/SpaceShooter/SatelliteShooter.cs
@@ -5,10 +5,10 @@ class SatelliteShooter: MonoBehaviour {
 
 	public void Start() {
 		Vector3 pos = this.gameObject.transform.position;
-		iTween.MoveTo(this.gameObject, iTween.Hash("position", new Vector3(pos.x,
-		                                                                   pos.y + 80,
+		iTween.MoveTo(this.gameObject, iTween.Hash("position", new Vector3(pos.x + 5,
+		                                                                   pos.y + 72,
 		                                                                   pos.z),
-		                                           "time", 30f));
+		                                           "time", 20f));
 	}
 
 };
