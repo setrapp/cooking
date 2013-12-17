192e7611a66e50db8ba6be460a5b476ff786dcfd
Robert  Lumsden <rml8300@rit.edu>
2013-12-17 03:43:18 -0500

    Bob's Scene: MainBob, New world geometry, sun, satellite, Rel materials for all

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+
+	public float rotSpeed;
+
+	// Use this for initialization
+	void Start () {
+		rotSpeed = 0.5f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(Vector3.up, rotSpeed * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/Sunscript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sunscript : MonoBehaviour {
+
+	public float sunSpeed;
+
+	// Use this for initialization
+	void Start () {
+
+		sunSpeed = 1f;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		transform.Translate(Vector3.up * sunSpeed * Time.deltaTime);
+	}
+}
